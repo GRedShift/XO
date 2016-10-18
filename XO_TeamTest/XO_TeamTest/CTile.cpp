614e51879c2c5a35b7f2dc41e054bb124ad58590
AIUbi <shadown@mail.ru>
2016-10-18 07:48:56 +0300

    File fix

---
+++ XO_TeamTest/XO_TeamTest/CTile.cpp
@@ -0,0 +1,18 @@
+#include "CTile.h"
+
+CTile::CTile()
+{
+}
+
+CTile::~CTile()
+{
+}
+
+void CTile::setTexture(sf::Texture * _texture)
+{
+}
+
+sf::Texture * CTile::getTexture()
+{
+	return nullptr;
+}
