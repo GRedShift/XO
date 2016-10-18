d7f659cc48b3d8980ee2b2210b80724e68ff8f11
AIUbi <shadown@mail.ru>
2016-10-18 06:44:43 +0300

    upd 0.0.2 -- Project commit

---
+++ XO_TeamTest/XO_TeamTest/Source.cpp
@@ -0,0 +1,22 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode::getDesktopMode(), "XO Team test");
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed) window.close();
+		}
+
+		window.clear(sf::Color::Magenta);
+
+		window.display();
+	}
+	
+	return 0;
+}
